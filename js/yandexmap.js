8552f639b06bfa061b81ca30d901ef77f3b592c8
patychkina <patychkina@bk.ru>
2024-12-17 20:06:42 +0300

    Add back files

---
+++ js/yandexmap.js
@@ -0,0 +1,25 @@
+ymaps.ready(init);
+function init() {
+    var map = new ymaps.Map("map", {
+        center: [47.215572, 38.916527], 
+        zoom: 17, 
+        controls: ['zoomControl', 'geolocationControl'] 
+    });
+
+    // Добавляем метку
+    var placemark = new ymaps.Placemark(
+        [47.215572, 38.916527],
+        {
+            hintContent: "Кузнечная, 6",
+            balloonContent: "Ростовская область, г. Таганрог, ул. Кузнечная, 6"
+        },
+        {
+            iconLayout: 'default#image',
+            iconImageHref: 'assets/icons/custom-marker.png',
+            iconImageSize: [40, 40],
+            iconImageOffset: [-20, -40]
+        }
+    );
+
+    map.geoObjects.add(placemark);
+}
